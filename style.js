9272e23e8fff5437afd49586c7ca7c1d1e1616a9
varun <varunverma01110@gmail.com>
2026-09-18 17:13:33 +0530

    update message

--- style.js
+++ style.js
@@ -1,22 +1,25 @@
-let badges = document.querySelectorAll(".skill-badge");
-let currentIndex = 0;
-
-badges[currentIndex].classList.add("active");
-
-setInterval(function() {
-    badges[currentIndex].classList.remove("active");
-
-    currentIndex = currentIndex + 1;
-
-    if (currentIndex >= badges.length) {
-        currentIndex = 0;
-    }
-
-    badges[currentIndex].classList.add("active");
-}, 1000);
-
 let cordHandle = document.getElementById("cordHandle");
+console.log(cordHandle);
 
 cordHandle.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
+});
+
+let logoIcon = document.getElementsByClassName("logo-icon")[0];
+console.log(logoIcon);
+
+logoIcon.addEventListener("click", function() {
+    logoIcon.classList.toggle("active");
+});
+
+let hamburger = document.querySelector(".hamburger");
+let sidebar = document.querySelector(".sidebar");
+
+hamburger.addEventListener("click", function() {
+    sidebar.classList.toggle("show");
+});
+let sidebarClose = document.getElementById("sidebarClose");
+
+sidebarClose.addEventListener("click", function() {
+    sidebar.classList.remove("show");
 });
